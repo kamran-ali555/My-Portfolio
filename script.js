edffecfcc03a19d56b52e9e40be9602cbc6345a7
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-01-06 12:21:32 +0500

    forth commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const page1 = document.querySelector('#home-one');
+const page2 = document.querySelector('#home-two');
+let isPage1Visible = true;
+
+// Function to toggle pages
+function togglePages() {
+  if (isPage1Visible) {
+    page1.style.transform = 'translateX(-1440px)';
+    page2.style.transform = 'translateX(0)';
+  } else {
+    page1.style.transform = 'translateX(0)';
+    page2.style.transform = 'translateX(-1440px)';
+  }
+  isPage1Visible = !isPage1Visible;
+}
+
+// Automatically toggle pages every 5 seconds
+setInterval(togglePages, 5000);
